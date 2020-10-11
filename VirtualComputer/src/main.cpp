935fbde0d1c2d20fe1c527dbdfedfba2126e2364
עמר פריאל <omer2079@gmail.com>
2020-10-11 14:29:29 +0300

    wip

--- VirtualComputer/src/main.cpp
+++ VirtualComputer/src/main.cpp
@@ -2,8 +2,10 @@
 
 int main()
 {
+    Utils::Debug::DebugTrace::BeginSession();
     std::cout << "Hello World\n";
 
     std::cout << "Press any key to continue . . .";
     std::cin.get();
+    Utils::Debug::DebugTrace::EndSession();
 }
--- VirtualComputer/src/pch.h
+++ VirtualComputer/src/pch.h
@@ -5,3 +5,10 @@
 
 // Data Structures
 #include <string>
+
+// Utils
+#include "Utils/Debug/Logger.h"
+#include "Utils/Debug/DebugTimer.h"
+
+using Logger = Utils::Debug::Logger;
+using DebugTimer = Utils::Debug::DebugTimer;
