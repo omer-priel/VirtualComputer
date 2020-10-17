9fd67a478325f989731939ca9ab1978dfc7169d8
Omer Priel <omer2079@gmail.com>
2020-10-17 20:29:38 +0300

    wip

--- VirtualComputer/src/main.cpp
+++ VirtualComputer/src/main.cpp
@@ -3,51 +3,6 @@
 #include "Drive.h"
 #include "Directory.h"
 
-// Drive = file
-// Path = Drive:/DirectoryNames.../FileName
-
-/*
-    Names:
-        Drive, Directory, File, Path
-        Entity = Directory or File.
-        Chank = Area of bytes on the drive.
-        Location = ChankId
-    Settings:
-        MAX_DRIVES 26
-        MAX_DIRECTORIES = 255
-        MAX_FILES = 255
-        MAX_ENTITY_NAME = 22
-        POINTER sizeof(unsigned int)
-        MAX_FILE_SIZE sizeof(unsigned int)
-        CHANK_SIZE = Directory Size
-    Drive:
-        Drive = CHANK_SIZE - MAX_ENTITY_NAME
-            like Directory without CHANK_SIZE
-
-        Directory = CHANK_SIZE
-            DirectoryName [MAX_ENTITY_NAME]
-            DirectoriesCount MAX_DIRECTORIES
-            DirectoriesLocations POINTER[MAX_DIRECTORIES]
-            FilesCount MAX_FILES
-            FilesLocations [MAX_FILES]
-
-        File = CHANK_SIZE
-            FileName [MAX_ENTITY_NAME]
-            FileSize MAX_FILE_SIZE
-            FileBody [CHANK_SIZE - 4]
-            FileBody
-                do
-                    BodyChank [CHANK_SIZE - 4]
-                    if (File <= CHANK_SIZE - 4)
-                        4 bytes of 0
-                    else
-                        PointerToNextChank
-                while (FileSize <= CHANK_SIZE - 4);
-        DeletedMemoryList = CHANK_SIZE
-            ChanksPointers [CHANK_SIZE - 4]
-            NextDeletedMemoryList ChankLocation
-*/
-
 void PrintDirectoryBody(DirectoryBody* directoryBody)
 {
     std::cout << "Directories:\n";
